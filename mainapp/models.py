2623f633dd4da273ac0a9349c61e66cea5fe9109
Artem Dronkin <adronkin@gmail.com>
2019-09-29 09:43:21 +0300

    +.gitignore and modified models.py (sorry)

--- mainapp/models.py
+++ mainapp/models.py
@@ -3,12 +3,12 @@ from django.db import models
 
 class Category(models.Model):
 	"""Класс описывающий вид животного"""
-    name = models.CharField(verbose_name='вид', max_length=64, unique=True)  # кошка или собака
-    description = models.TextField(verbose_name='описание', blank=True)  # нужно ли?
-    is_active = models.BooleanField(verbose_name='активность', default=True)
+	name = models.CharField(verbose_name='вид', max_length=64, unique=True)  # кошка или собака
+	description = models.TextField(verbose_name='описание', blank=True)  # нужно ли?
+	is_active = models.BooleanField(verbose_name='активность', default=True)
 
-    def __str__(self):
-        return self.name
+	def __str__(self):
+		return self.name
 
 
 class PetGender(models.Model):
@@ -31,11 +31,11 @@ class PetSize(models.Model):
 
 class PetWool(models.Model):
 	"""Класс описывающий длину шерсти животного"""
-	wool_lenght = models.CharField(verbose_name='длина шерсти', max_length=16, unique=True)
+	wool_length = models.CharField(verbose_name='длина шерсти', max_length=16, unique=True)
 	is_active = models.BooleanField(verbose_name='активность', default=True)
 
 	def __str__(self):
-		return self.wool_lenght
+		return self.wool_length
 
 
 class PetColor(models.Model):
@@ -56,28 +56,28 @@ class PetCharacter(models.Model):
 		return self.character
 
 
-# добавить особенности (теги), например нет глаза и ТД
+# добавить особенности (теги), например нет глаза и т.д.
 
 
 class Pet(models.Model):
 	"""Класс описывающий животного"""
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    pet_name = models.CharField(verbose_name='имя', max_length=64)
-    pet_gender = models.ForeignKey(PetGender, on_delete=models.CASCADE)
-    age = models.PositiveIntegerField(verbose_name='возраст (лет)', default=0)
-    month = models.PositiveIntegerField(verbose_name='возраст (мес)', default=0)
-    pet_size = models.ForeignKey(PetSize, on_delete=models.CASCADE)
-    pet_wool_lenght = models.ForeignKey(PetWool, on_delete=models.CASCADE)
-    pet_color = models.ForeignKey(PetColor, on_delete=models.CASCADE)
-    pet_character = models.ForeignKey(PetCharacter, on_delete=models.CASCADE)
-    description = models.TextField(verbose_name='описание', blank=True)
-    image = models.ImageField(upload_to='pet_photo', blank=True)
-    is_active = models.BooleanField(verbose_name='активность', default=True)
+	category = models.ForeignKey(Category, on_delete=models.CASCADE)
+	pet_name = models.CharField(verbose_name='имя', max_length=64)
+	pet_gender = models.ForeignKey(PetGender, on_delete=models.CASCADE)
+	age = models.PositiveIntegerField(verbose_name='возраст (лет)', default=0)
+	month = models.PositiveIntegerField(verbose_name='возраст (мес)', default=0)
+	pet_size = models.ForeignKey(PetSize, on_delete=models.CASCADE)
+	pet_wool_length = models.ForeignKey(PetWool, on_delete=models.CASCADE)
+	pet_color = models.ForeignKey(PetColor, on_delete=models.CASCADE)
+	pet_character = models.ForeignKey(PetCharacter, on_delete=models.CASCADE)
+	description = models.TextField(verbose_name='описание', blank=True)
+	image = models.ImageField(upload_to='pet_photo', blank=True)
+	is_active = models.BooleanField(verbose_name='активность', default=True)
 
-    def __str__(self):
-        return self.pet_name
+	def __str__(self):
+		return self.pet_name
 
-    # Убираем из каталога неактивные объявления
-    @staticmethod
-    def get_items():
-        return Pet.objects.filter(is_active=True, category__is_active=True).order_by('category', 'name')
+	# Убираем из каталога неактивные объявления
+	@staticmethod
+	def get_items():
+		return Pet.objects.filter(is_active=True, category__is_active=True).order_by('category', 'name')
