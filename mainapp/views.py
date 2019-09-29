cd1bb59b3680426f49eefa82b834172b759eb38c
SKov82 <skov82@ya.ru>
2019-09-29 16:54:54 +0300

    Поправил модель mainapp - создал родительский класс, реализовал модель изображений и наследования. Подключил админ-панель, зарегал модели. Поправил главный диспетчер url, добавил диспетчер url mainapp. Реализовал и подключил медиа-контент. Создал суперпользователя, выполнил миграции, немного наполнил БД для проверки.

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,12 @@
+from django.views.generic import TemplateView
 from django.shortcuts import render
 
-# Create your views here.
+
+class Index(TemplateView):
+    """ Главная страница """
+    template_name = 'mainapp/index.html'
+
+
+class Contact(TemplateView):
+    """ Страница контактов интернет-магазина """
+    template_name = 'mainapp/contact.html'
