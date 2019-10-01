c553d806e854a85dc9dd3c7a21f377beac826a16
Maksim Kokarev <hurtmind@mail.ru>
2019-10-02 00:23:11 +0300

    Доделаны все templates. Другие файлы вроде не трогал в этом коммите

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import TemplateView
+from django.views.generic.base import TemplateView
 from django.shortcuts import render
 
 
@@ -20,6 +20,12 @@ class About(TemplateView):
 class Cats(TemplateView):
     """Страница кошек"""
     template_name = 'mainapp/cats.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'cats'
+        context['href'] = 'main:cats'
+        return context
 
 
 class Dogs(TemplateView):
