323499a4acd918a5d2386798e97f940f1622f5fd
Alyonkazz <alyonkazz@gmail.com>
2019-10-01 18:54:14 +0300

    mainapp.views, pet_card:     - вывод возраста

--- mainapp/views.py
+++ mainapp/views.py
@@ -13,6 +13,7 @@ class Contact(TemplateView):
     """ Страница контактов интернет-магазина """
     template_name = 'mainapp/contact.html'
 
+
 def pet_list(request):
     title = 'СПИСОК ПИТОМЦЕВ'
     pets = Pet.objects.all()
@@ -26,10 +27,25 @@ def pet_list(request):
 def pet_card(request, pk):
     pet = get_object_or_404(Pet, pk=pk)
 
+    year_output = 'год(-а)'
+    if pet.age == 1:
+        year_output = 'год'
+    elif 4 >= pet.age > 2:
+        year_output = 'года'
+    elif 20 >= pet.age >= 5:
+        year_output = 'лет'
+
+    if pet.month == 1:
+        month_output = 'месяц'
+    elif 4 >= pet.month > 2:
+        month_output = 'месяца'
+    elif 12 >= pet.month >= 5 or pet.month == 0:
+        month_output = 'месяцев'
+
     context = {
         'title': 'карточка питомца',
-        # 'catalog_menu': get_catalog_menu(),
-        # 'category': pet.pet_category_type,
         'pet': pet,
+        'year_output': year_output,
+        'month_output': month_output
     }
     return render(request, 'mainapp/pet_card.html', context)
