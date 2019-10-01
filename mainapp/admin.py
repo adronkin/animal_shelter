725ca83ee5ea3202a358db61e83f6d5ec8650cf2
Artem Dronkin <adronkin@gmail.com>
2019-10-01 06:56:53 +0300

    merge with alyonkazz

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 
 from .models import (
-    Picture, City, Shelter, Donate, Social, PetCategory, PetStatus, PetBreed,
-    PetGender, PetSize, PetWool, PetColor, PetCharacter, Pet
+    Picture, Category_type, Category_health, PetGender, PetSize, PetWool, PetColor, PetCharacter, PetCity, Pet
 )
 
 
@@ -13,33 +12,14 @@ class PictureInline(admin.TabularInline):
 
 
 @admin.register(Pet)
-class PetAdmin(admin.ModelAdmin):
+class ProductAdmin(admin.ModelAdmin):
     """ Набор изображений для отдельно взятого питомца """
     inlines = (PictureInline,)
 
 
-class SocialInline(admin.TabularInline):
-    """ ссылки на соц.сети """
-    model = Social
-    fk_name = 'obj'
-
-
-class DonateInline(admin.TabularInline):
-    """ реквизиты для финансовой помощи """
-    model = Donate
-    fk_name = 'account'
-
-
-@admin.register(Shelter)
-class ShelterAdmin(admin.ModelAdmin):
-    """ Подключаем к питомнику соц.сети, донаты, картинки """
-    inlines = (SocialInline, DonateInline, PictureInline,)
-
-
-admin.site.register(City)
-admin.site.register(PetCategory)
-admin.site.register(PetStatus)
-admin.site.register(PetBreed)
+admin.site.register(Category_health)
+admin.site.register(Category_type)
+admin.site.register(PetCity)
 admin.site.register(PetGender)
 admin.site.register(PetSize)
 admin.site.register(PetWool)
