cf06404da74c3bec80bf690ca21d2c121b9e11ff
Maksim Kokarev <hurtmind@mail.ru>
2019-10-01 10:03:25 +0300

    test commit

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 
+<<<<<<< HEAD
 class Core(models.Model):
     """ Класс ядро - родитель всех классов-моделей """
     class Meta:
@@ -30,43 +31,95 @@ class Picture(Core):
     related_obj = models.ForeignKey(Core, verbose_name='изображения', null=True, blank=True, related_name='images', on_delete=models.CASCADE)
 
 
-class Category(Core):
-    """Класс описывающий вид животного"""
-
-
-class PetGender(Core):
-    """Класс описывающий пол животного"""
-
-
-class PetSize(Core):
-    """Класс описывающий размеры животного"""
-
-
-class PetWool(Core):
-    """Класс описывающий длину шерсти животного"""
-
-
-class PetColor(Core):
-    """Класс описывающий цвет животного"""
-
-
-class PetCharacter(Core):
-    """Класс описывающий характер животнго"""
-
-
 # добавить особенности (теги), например нет глаза и т.д.
 
 
 class Pet(Core):
     """Класс описывающий животного"""
     pet_category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+# =======
+class Category(models.Model):
+    """Класс описывающий вид животного"""
+    name = models.CharField(verbose_name='вид', max_length=64, unique=True)  # кошка или собака
+    description = models.TextField(verbose_name='описание', blank=True)  # нужно ли?
+    is_active = models.BooleanField(verbose_name='активность', default=True)
+
+    def __str__(self):
+        return self.name
+
+
+class PetGender(models.Model):
+	"""Класс описывающий пол животного"""
+	gender = models.CharField(verbose_name='пол', max_length=16, unique=True)
+	is_active = models.BooleanField(verbose_name='активность', default=True)
+
+	def __str__(self):
+		return self.gender
+
+
+class PetSize(models.Model):
+	"""Класс описывающий размеры животного"""
+	size = models.CharField(verbose_name='размер', max_length=16, unique=True)
+	is_active = models.BooleanField(verbose_name='активность', default=True)
+
+	def __str__(self):
+		return self.size
+
+
+class PetWool(models.Model):
+	"""Класс описывающий длину шерсти животного"""
+	wool_lenght = models.CharField(verbose_name='длина шерсти', max_length=16, unique=True)
+	is_active = models.BooleanField(verbose_name='активность', default=True)
+
+	def __str__(self):
+		return self.wool_lenght
+
+
+class PetColor(models.Model):
+	"""Класс описывающий цвет животного"""
+	color = models.CharField(verbose_name='цвет', max_length=32, unique=True)
+	is_active = models.BooleanField(verbose_name='активность', default=True)
+
+	def __str__(self):
+		return self.color
+
+
+class PetCharacter(models.Model):
+	"""Класс описывающий характер животнго"""
+	character = models.CharField(verbose_name='характер', max_length=32, unique=True)
+	is_active = models.BooleanField(verbose_name='активность', default=True)
+
+	def __str__(self):
+		return self.character
+
+
+# добавить особенности (теги), например нет глаза и ТД
+
+class Pet(models.Model):
+    """Класс описывающий животного"""
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    pet_name = models.CharField(verbose_name='имя', max_length=64)
+>>>>>>> mainapp models.py
     pet_gender = models.ForeignKey(PetGender, on_delete=models.CASCADE)
     age = models.PositiveIntegerField(verbose_name='возраст (лет)', default=0)
     month = models.PositiveIntegerField(verbose_name='возраст (мес)', default=0)
     pet_size = models.ForeignKey(PetSize, on_delete=models.CASCADE)
+<<<<<<< HEAD
     pet_wool_length = models.ForeignKey(PetWool, on_delete=models.CASCADE)
     pet_color = models.ForeignKey(PetColor, on_delete=models.CASCADE)
     pet_character = models.ForeignKey(PetCharacter, on_delete=models.CASCADE)
+=======
+    pet_wool_lenght = models.ForeignKey(PetWool, on_delete=models.CASCADE)
+    pet_color = models.ForeignKey(PetColor, on_delete=models.CASCADE)
+    pet_character = models.ForeignKey(PetCharacter, on_delete=models.CASCADE)
+    description = models.TextField(verbose_name='описание', blank=True)
+    image = models.ImageField(upload_to='pet_photo', blank=True)
+    is_active = models.BooleanField(verbose_name='активность', default=True)
+
+    def __str__(self):
+        return self.pet_name
+>>>>>>> mainapp models.py
 
     # Убираем из каталога неактивные объявления
     @staticmethod
