37d5625bd1089b88245dfc69747a69fb5b283c35
Alyonkazz <alyonkazz@gmail.com>
2019-10-07 22:52:59 +0300

    Register:     - mainmenu.html     - authapp:         - login.html         - register.html         - base.html         - forms.py         - urls.py         - views.py

--- authapp/urls.py
+++ authapp/urls.py
@@ -1,7 +1,11 @@
+import authapp.views as authapp
+
 from django.contrib.auth import views
 from django.urls import path
+
 
 urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('register/', authapp.register, name='register'),
 ]
