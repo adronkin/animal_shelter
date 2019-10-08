f4813a7feb6024746b2967b15ac50b17c4a89178
Alyonkazz <alyonkazz@gmail.com>
2019-10-08 17:51:36 +0300

    Подтверждение почты:     - settings.py     - authapp:         - verification.html         - models.py         - forms.py         - views.py         - urls.py

--- authapp/forms.py
+++ authapp/forms.py
@@ -1,6 +1,11 @@
+import hashlib
+import random
+
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+
+from authapp.models import ActivateUser
 
 
 class RegisterForm(UserCreationForm):
@@ -9,3 +14,15 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'password1', 'password2', 'email']
+
+    def save(self):
+        user = super(RegisterForm, self).save()
+
+        user.is_active = False
+        user.save()
+        salt = hashlib.sha1(str(random.random()).encode('utf8')).hexdigest()[:6]
+        activate_user = ActivateUser.objects.create(user_id=user.id)
+        activate_user.activation_key = hashlib.sha1((user.email + salt).encode('utf8')).hexdigest()
+        activate_user.save()
+
+        return user
