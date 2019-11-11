3010c46ceb4cfc92fe9f5f3213269d0cbbdeebe2
Alyonkazz <alyonkazz@gmail.com>
2019-11-11 16:45:21 +0300

    создан личный кабинет приюта

--- shelteradminapp/urls.py
+++ shelteradminapp/urls.py
@@ -5,6 +5,14 @@ from mainapp.models import Picture
 app_name = 'shelteradminapp'
 
 urlpatterns = [
+    path('shelter/user/office/<int:pk>/', shelteradminapp.ShelterOffice.as_view(), name='shelter_office'),
+
+    path('shelter/create/', shelteradminapp.ShelterCreate.as_view(), name='shelter_create'),
+    path('shelter/read/<int:pk>/', shelteradminapp.ShelterDetail.as_view(), name='shelter_detail'),
+    # path('shelter/update/<int:pk>/', adminapp.ShelterUpdate.as_view(), name='shelter_update'),
+    # path('shelter/update/<int:pk>/pet/create/', adminapp.PetCreateInShelter.as_view(), name='pet_create_in_shelter'),
+    # path('shelter/delete/<int:pk>/', adminapp.ShelterDelete.as_view(), name='shelter_delete'),
+
     # path('pet_list/', adminapp.PetList.as_view(), name='pet_list'),
     # path('pet/create/', adminapp.PetCreate.as_view(), name='pet_create'),
     # path('pet/create/', adminapp.PetCreateInShelter.as_view(), name='pet_create_in_shelter'),
@@ -12,13 +20,6 @@ urlpatterns = [
     # path('pet/update/<int:pk>/', adminapp.PetUpdate.as_view(), name='pet_update'),
     # path('pet/delete/<int:pk>/', adminapp.PetDelete.as_view(), name='pet_delete'),
 
-    # path('shelter/list/', adminapp.ShelterList.as_view(), name='shelter_list'),
-    path('shelter/create/', shelteradminapp.ShelterCreate.as_view(), name='shelter_create'),
-    # path('shelter/read/<int:pk>/', adminapp.ShelterDetail.as_view(), name='shelter_detail'),
-    # path('shelter/update/<int:pk>/', adminapp.ShelterUpdate.as_view(), name='shelter_update'),
-    # path('shelter/update/<int:pk>/pet/create/', adminapp.PetCreateInShelter.as_view(), name='pet_create_in_shelter'),
-    # path('shelter/delete/<int:pk>/', adminapp.ShelterDelete.as_view(), name='shelter_delete'),
-    #
     # path('pet/update/<int:pk>/create/image/', adminapp.ImageCreatePet.as_view(model=Picture), name='image_create_pet'),
     # path('shelter/update/<int:pk>/create/image/', adminapp.ImageCreateShelter.as_view(model=Picture), name='image_create_shelter'),
     # path('update/image/<int:pk>/', adminapp.ImageUpdate.as_view(model=Picture), name='image_update'),
--- shelteradminapp/views.py
+++ shelteradminapp/views.py
@@ -1,9 +1,35 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView, TemplateView, DetailView
+from django.shortcuts import get_object_or_404
 
 from mainapp.models import Shelter
 from shelteradminapp.forms import ShelterUserUpdateForm
+
+# TODO В главном меню "Создать приют" не заменяется на "Личный кабинет приюта"
+# TODO Приют сохраняется в Core дважды - исправить
+# TODO В ShelterDetail при отсутсвии логитипа возникает ошибка - добавить дефолтное лого
+# TODO ShelterUpdate
+# TODO ShelterDelete
+# TODO отредактировать ЛК приюта
+# TODO PetCreate
+# TODO PetDetail
+# TODO PetUpdate
+# TODO PetDelete
+
+
+class ShelterOffice(DetailView):
+    model = User
+    template_name = 'shelteradminapp/shelter_office.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Личный кабинет приюта'
+        # context['my_shelter'] = User.
+        # a = Shelter.objects.filter(user_id=4)
+        # context['my_kwargs'] = self.get_object(a)
+        return context
 
 
 class ShelterCreate(CreateView):
@@ -21,3 +47,18 @@ class ShelterCreate(CreateView):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Создание приюта'
         return context
+
+    def get_success_url(self):
+        return reverse('shelteradmin:shelter_detail', args=(self.object.id,))
+
+
+class ShelterDetail(DetailView):
+    """Выводит описание приюта"""
+    model = Shelter
+    template_name = 'shelteradminapp/shelter_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Приют'
+        return context
+
